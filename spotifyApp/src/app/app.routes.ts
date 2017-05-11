7cdd21c31d401093444b39d19d33300d6631fdc2
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-11 12:08:58 +0200

    feat(*): added search component

--- spotifyApp/src/app/app.routes.ts
+++ spotifyApp/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 //import components
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 const app_routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'search/:term', component: SearchComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
