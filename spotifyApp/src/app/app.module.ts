fde8578d8d49a3d449bf106bea5541433bdfc60d
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-11 10:44:13 +0200

    feat(app.module.ts): Added the artist component to the application

--- spotifyApp/src/app/app.module.ts
+++ spotifyApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
