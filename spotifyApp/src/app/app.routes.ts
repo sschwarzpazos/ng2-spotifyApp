29cfcc07bdea57b74142d4cd8413c082d6c854cd
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-11 12:12:01 +0200

    feat(*): added artist component

--- spotifyApp/src/app/app.routes.ts
+++ spotifyApp/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 //import components
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 const app_routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search/:term', component: SearchComponent },
+  { path: 'artist/:id', component: ArtistComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
